28617d13bb259531751806c315964a8494681c17
Jérôme Wassenberg <wassenbeje@etml.educanet2.ch>
2017-05-30 16:32:15 +0200

    S1 J2
    Page d'accueil finie
    MCD en cours de création

--- Site/src/php/students.php
+++ Site/src/php/students.php
@@ -3,7 +3,7 @@
  * ETML
  * Auteur : Jérôme Wassenberg
  * Date : 29.05.2017
- * Description : Page d'accueil du site
+ * Description : Page de visualisation des élèves
  */
 include 'includes/header.php'
 ?>
@@ -12,13 +12,42 @@ include 'includes/header.php'
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <title>Accueil</title>
+        <title>Elèves</title>
 
         <link rel="stylesheet" type="text/css" href="../../ressources/css/common.css">
-        <link rel="stylesheet" type="text/css" href="../../ressources/css/index.css">
+        <link rel="stylesheet" type="text/css" href="../../ressources/css/common.css">
     </head>
 
     <body>
+        <section>
+            <!--Titre-->
+            <h1>Elèves</h1>
 
+            <?php
+            #Préparation des données
+            $result1 = $PDOLink->prepareData($req1);
+
+            #Affichage des informations des élèves
+            foreach($result1 as $display)
+            {
+                ?>
+                <a href="viewDetails.php?id=<?php echo $display['idStudent']?>">
+                    <div id="cour">
+                        <div>
+                            <div id="logo">
+                                <img src="../../ressources/images/logo_student.png">
+                            </div>
+                            <div id="name">
+                                <p><?php echo $display['stuFirstname']?></p>
+                                <p><?php echo $display['stuName']?></p>
+
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <?php
+            }
+            ?>
+        </section>
     </body>
 </html>
